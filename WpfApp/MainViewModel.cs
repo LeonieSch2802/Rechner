2798d4ea362c3c55cc0897e41eabf887411e6c21
Peter Rasser <peter.rasser@hakzell.at>
2020-06-19 08:24:54 +0200

    bugfix

--- WpfApp/MainViewModel.cs
+++ WpfApp/MainViewModel.cs
@@ -1,13 +1,9 @@
 ﻿using MvvmCross.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace WpfApp
 {
-    class MainViewModel : MvxViewModel
+    public class MainViewModel : MvxViewModel
     {
         public string Title => "Rechenautomat";
     }
