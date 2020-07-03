e108a637e3007dbc4a47b3232ab86ab8d4a78d84
Leonie Schnitzler <lschnitzler@schueler.hakzell.at>
2020-07-03 07:51:04 +0200

    Design

--- WpfApp/MainViewModel.cs
+++ WpfApp/MainViewModel.cs
@@ -1,10 +1,23 @@
 ﻿using MvvmCross.ViewModels;
-
+using Rechner.Core;
 
 namespace WpfApp
 {
     public class MainViewModel : MvxViewModel
     {
+
+        Rechenautomat calc = new Rechenautomat();
         public string Title => "Rechenautomat";
+
+        public string Erkl => "Das ist ein Rechenautomat";
+
+        public string Calculation
+        {
+            get
+            {
+                return calc.NächsteBerechnung();
+            }
+        }
+
     }
 }
