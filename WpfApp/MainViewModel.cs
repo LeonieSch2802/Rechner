6414d220055affe8a2760d23f24c122b6abcb970
Leonie Schnitzler <lschnitzler@schueler.hakzell.at>
2020-07-03 08:50:00 +0200

    Richtig oder Falsch

--- WpfApp/MainViewModel.cs
+++ WpfApp/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
 using Rechner.Core;
 
 namespace WpfApp
@@ -7,6 +8,12 @@ namespace WpfApp
     {
 
         Rechenautomat calc = new Rechenautomat();
+
+        public MainViewModel()
+        {
+            this.CheckCommand = new MvxCommand(Check, CanCheck);
+        }
+
         public string Title => "Rechenautomat";
 
         public string Erkl => "Das ist ein Rechenautomat";
@@ -18,6 +25,70 @@ namespace WpfApp
                 return calc.NächsteBerechnung();
             }
         }
+
+        private string _userInput;
+
+        public string UserInput
+        {
+            get { return _userInput; }
+            set { _userInput = value;
+                RaisePropertyChanged(() => UserInput);
+                this.CheckCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private int _falsch = 0;
+
+        public string Falsch
+        {
+            get
+            {
+                return Convert.ToString(this._falsch);
+            }
+        }
+
+        private int _richtig = 0;
+        public string Richtig
+        {
+            get
+            {
+                return Convert.ToString(this._richtig);
+            }
+        }
+
+        /**
+         * commands
+         */
+
+        #region Commands
+        public MvxCommand CheckCommand { get; set; }
+        public void Check()
+        {
+            var zahl = Convert.ToInt32(this.UserInput);
+
+            var erg = this.calc.Prüfe(zahl);
+
+            RaisePropertyChanged(() => Calculation);
+
+            if(erg)
+            {
+                //richtig
+                this._richtig++;
+                RaisePropertyChanged(() => Richtig);
+            }
+            else
+            {
+                //falsch
+                this._falsch++;
+                RaisePropertyChanged(() => Falsch);
+            }
+        }
+
+        public bool CanCheck()
+        {
+            return this.UserInput.Trim().Length != 0;
+        }
+        #endregion
 
     }
 }
